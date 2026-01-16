8be4e44de8015906ac77d337530ae7c88494e468
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2026-01-16 16:38:43 -0600

    Update install-user.sh

--- install-user.sh
+++ install-user.sh
@@ -2,3 +2,4 @@ mkdir -p ~/.local/bin
 ln -s ~/.local/share/apps/gitpm/gitpm.py ~/.local/bin/gitpm
 chmod +x ~/.local/share/apps/gitpm/gitpm.py
 echo export PATH="$PATH:~/.local/bin" >> ~/.bashrc
+echo "Close all running terminals and run 'gitpm' once for tab completion to find it"
