56baa6c65139a3efc8c3d123311bbf1239b39402
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2026-01-16 16:25:31 -0600

    Update install-user.sh

--- install-user.sh
+++ install-user.sh
@@ -1,3 +1,3 @@
 mkdir -p ~/.local/bin
 ln -s ~/.local/share/apps/gitpm/gitpm.py ~/.local/bin/gitpm
-echo export PATH="$PATH:~/.local/bin" > ~/.bashrc
+echo export PATH="$PATH:~/.local/bin" >> ~/.bashrc
